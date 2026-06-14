00eeebfd547c0ac27bb29edcd2557201d0633bab
michael-byte84 <cardarelli.michael@gmail.com>
2026-06-14 15:07:31 +0200

    Update data.js

--- data.js
+++ data.js
@@ -7,25 +7,23 @@ const siteData = {
     news: [
         {
             data: "13 giugno 2026",
-            titolo: "Nuovo orario per la donazione di capi usati",
-            testo: "La raccolta di capi usati, per tutto il 2026, sarà effettuata solo dalle 08:00 alle 13:00."},
+            titolo: "Cambio orario donazione capi usati",
+            testo: "Per tutto il mese di giugno, l'orario per la donazione dei capi usati sarà in orario antimeridiano dalle 08:30 alle 13:00."},
         {
             data: "1 Luglio 2026",
             titolo: "Nuovo orario estivo",
-            testo: "Da oggi osserveremo il nuovo orario estivo: lun-ven 07:00 - 14:00, sab 07:00 - 13:00. Ad agosto saremo aperti solo nella
-            prima settimana.",}
-        {
-            
+            testo: "Da oggi osservemo il nuovo orario estivo: Lunedì - Sabato 08:30 - 13:00 | 15:30 - 19:30. Domenica chiuso.",
+        }
     ],
 
     // Aggiorna qui gli orari di apertura
-orario: [
+orari: [
         { giorno: "Lunedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
         { giorno: "Martedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
         { giorno: "Mercoledì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
         { giorno: "Giovedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
         { giorno: "Venerdì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Sabato", ore: "08:00 - 13:00" },
+        { giorno: "Sabato", ore: "08:00 - 13:00 | Chiuso" },
         { giorno: "Domenica", ore: "Chiuso" }
     ]
 };
