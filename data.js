03f6574f49f7d6b718de8e7cb3acff5739d61d6b
michael-byte84 <cardarelli.michael@gmail.com>
2026-07-01 10:38:13 +0200

    Adjust opening hours and fix text for announcements
    Updated opening hours and corrected text for summer schedule.

--- data.js
+++ data.js
@@ -12,23 +12,23 @@ const siteData = {
         {
             data: "1 luglio 2026",
             titolo: "Nuovo orario estivo",
-            testo: "Dal 1 luglio osserveremo il nuovo orario estivo: dal lunedì al sabato, dalle 07:00 alle 13:00. Domenica chiuso.",
+            testo: "Dal 1 luglio osserviamo il nuovo orario estivo: dal lunedì al sabato, dalle 07:00 alle 13:00. Domenica chiuso.",
         },
         {
             data: "1 agosto 2026",
             titolo: "Chiusura per ferie",
-            testo: "Vi informiamo che ad agosto saremo aperti esclusivamente la prima settimana, dal 1° all'8 compresi.",
+            testo: "Vi informiamo che ad agosto saremo aperti esclusivamente la prima settimana, dal 1° all'8 compreso.",
         },
     ],
 
     // Aggiorna qui gli orari di apertura
 orario: [
-        { giorno: "Lunedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Martedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Mercoledì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Giovedì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Venerdì", ore: "08:00 - 13:00 | 15:00 - 19:00" },
-        { giorno: "Sabato", ore: "08:00 - 13:00 | Chiuso" },
+        { giorno: "Lunedì", ore: "07:00 - 13:00 | Chiuso" },
+        { giorno: "Martedì", ore: "07:00 - 13:00 | Chiuso" },
+        { giorno: "Mercoledì", ore: "07:00 - 13:00 | Chiuso" },
+        { giorno: "Giovedì", ore: "07:00 - 13:00 | Chiuso" },
+        { giorno: "Venerdì", ore: "07:00 - 13:00 | Chiuso" },
+        { giorno: "Sabato", ore: "07:00 - 13:00 | Chiuso" },
         { giorno: "Domenica", ore: "Chiuso" }
     ]
 };
